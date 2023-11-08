08e897d59d3a1dae38efae37434cb54bd11bdc00
itou aiki <itouaiki46@gmail.com>
2023-11-08 15:15:43 +0900

    fix TaskAのコメント追加

--- 20231108/TaskA.cpp
+++ 20231108/TaskA.cpp
@@ -4,8 +4,9 @@ using namespace std;
 
 int main(void)
 {
+    // 0~10までの繰り返し
     for(int i = 0; i <= 10; i++)
     {
-        cout << i << endl;
+        cout << i << endl; // 現在の数を表示
     }    
 }
