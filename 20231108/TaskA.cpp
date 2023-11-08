38bf5b4521170db0094578e61bd622e307c54d6a
itou aiki <itouaiki46@gmail.com>
2023-11-08 16:11:58 +0900

    fix　TaskAの修正

--- 20231108/TaskA.cpp
+++ 20231108/TaskA.cpp
@@ -7,8 +7,10 @@ int main(void)
     // 0~10までの繰り返し
     for(int i = 0; i <= 10; i++)
     {
-        cout << i << endl; // 現在の数を表示
+        cout << i << "\n"; // 現在の数を表示
     }
+
+    cout << endl;
 
     return 0;
 }
