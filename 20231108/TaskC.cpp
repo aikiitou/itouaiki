f05b0463fa68c94460761c93520fdeb4f2adeb28
itou aiki <itouaiki46@gmail.com>
2023-11-08 15:27:51 +0900

    fix TaskCの修正
    コメントの追加

--- 20231108/TaskC.cpp
+++ 20231108/TaskC.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main(void)
 {
-    int data[10] = {1,5,9,7,2,4,6,3,8,0};
+    int data[10] = {1,5,9,7,2,4,6,3,8,0}; // 10個の乱数が入った配列
 
+    // 0から配列の長さ分繰り返すループ
     for(int i = 0; i < sizeof(data)/sizeof(int); i++)
     {
-        cout << data[i] << endl;
+        cout << data[i] << endl; // 配列のi番目を表示
     }
 
     return 0;
