95a2ca0d86f4429fe662588581805a3abbab218e
itou aiki <itouaiki46@gmail.com>
2023-11-15 15:28:38 +0900

    add TaskAの追加
    0～10で整列された配列をシャッフルしてtargetのインデックスを出力するプログラム

---
+++ 20231115/TaskA.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std; 
+
+int main(void)
+{
+    int data[] = {0,1,2,3,4,5,6,7,8,9,10};
+    int size = sizeof(data) / sizeof(data[0]);
+
+    srand(time(NULL));
+    // dataのシャッフル
+    for(int i = size - 1; i > 0; i--)
+    {
+        int random = rand() % (i + 1);
+
+        int temp = data[i];
+        data[i] = data[random];
+        data[random] = temp;
+    }
+
+    int result = -1;
+    int target = 5;
+
+    for(int i = 0; i < size; i++)
+    {
+        if(data[i] == target)
+        {
+            result = i;
+            break;
+        }
+    }
+
+    cout << result;
+    cout << endl;
+}
