9194413eb8e57496422937165e454a4cfb68db7d
itou aiki <itouaiki46@gmail.com>
2023-11-15 15:49:05 +0900

    add TaskBの追加
    100個の乱数が入った配列をソートするプログラム

---
+++ 20231115/TaskB.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <random>
+#include <time.h>
+
+using namespace std;
+
+int main(void)
+{
+    int data[100];
+    int size = sizeof(data) / sizeof(data[0]);
+
+    srand(time(NULL));
+
+    //100個の乱数を配列にいれる
+    for(int i = 0; i < size; i++)
+    {
+        data[i] = rand();
+    }
+
+    for(int i = 0; i < size; ++i)
+    {
+        for(int j = size; j >= i + 1; --j)
+        {
+            int valueA = data[i];
+            int valueB = data[j];
+
+            if(valueA > valueB)
+            {
+                int max = valueA;
+                int min = valueB;
+
+                data[i] = min;
+                data[j] = max;
+            }
+        }
+    }
+
+    for(int i = 0; i < size; ++i)
+    {
+        printf("%d\n",data[i]);
+    }
+}
